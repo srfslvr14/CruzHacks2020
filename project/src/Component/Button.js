13922336daf37b1fbb01bba69440889e764888bd
Spencer <Spencerman@comcast.net>
2020-01-18 14:59:10 -0800

    button.js made

---
+++ project/src/Component/Button.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+function Button() {
+    return (
+        <div class="position-relative">
+                <div class="btn-group" role="group" aria-label="buttons">
+                    <div>
+                    <a class="btn btn-secondary btn-lg" href="https://media1.giphy.com/media/ejm8tkFonl8o8/giphy.gif"
+                    role="button">HOME</a>
+                    <a class="btn btn-success btn-lg" href="https://media1.giphy.com/media/ejm8tkFonl8o8/giphy.gif"
+                    role="button">LAND</a>
+                    <a class="btn btn-primary btn-lg" href="https://media1.giphy.com/media/ejm8tkFonl8o8/giphy.gif"
+                    role="button">SEA</a>
+                    <a class="btn btn-info btn-lg" href="https://media1.giphy.com/media/ejm8tkFonl8o8/giphy.gif"
+                    role="button">SKY</a>
+                    <a class="btn btn-danger btn-lg" href="https://tinyurl.com/rcpd8eb"
+                    role="button">DONATE</a>
+                    </div>
+                </div>
+            </div>
+    );
+}
+export default Button
+            
