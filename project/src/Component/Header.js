1ab71d13348520b5d02eb710adeace69fb47eb01
Christina Luong <cluong7@ucsc.edu>
2020-01-18 00:31:14 -0800

    split header into one file

---
+++ project/src/Component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div class = "Actual_header">
+          <h1>
+            Header
+          </h1>
+          <p>Headers are dumb</p>
+        </div>
+    );
+}
+export default Header
