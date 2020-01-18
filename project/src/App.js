43b74b30f3c3bcc3fc00329ec8c2af51cdc4c64e
Jevin Edric Olano <jolano@ucsc.edu>
2020-01-17 22:11:24 -0800

    Dank memes

--- project/src/App.js
+++ project/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Dank memes
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.omfgdogs.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Justice for the health of earth
         </a>
       </header>
     </div>
