4911cd4d8be3cdcd62015d0e1e0a0eeb4b5d03e2
Spencer <Spencerman@comcast.net>
2020-01-18 04:35:28 -0800

    card and body .js

---
+++ project/src/Component/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Component } from 'react';
+import Cards from '../Component/Cards';
+
+function Body() {
+    return(
+        <Cards />
+    );
+}
+export default Body
